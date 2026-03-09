a2e161073f11d48c4220ddf9c5a45ec0304cd975
Rahavy <114474771+Rahavy@users.noreply.github.com>
2026-03-09 19:02:21 +0000

    added auto threshold to img 2 and removed imgs 3 and 4

--- opencv_starter.py
+++ opencv_starter.py
@@ -55,26 +55,18 @@ print("auto thresholding", threshold)
 
 #implement thresholding ourselves using loops (soooo slow in python)
 before = time.time()
-#thresh = 100
-#for x in range(0, img.shape[0]):
-#    for y in range(0, img.shape[1]):
-#        if img[x,y] > thresh:
-#            img[x,y] = 255
-#        else:
-#            img[x,y] = 0
+for x in range(0, img.shape[0]):
+    for y in range(0, img.shape[1]):
+        if img[x,y] > threshold:
+            img[x,y] = 255
+        else:
+            img[x,y] = 0
 after = time.time()
 print("Time taken to process hand coded thresholding: " + str(after-before))
 cv.imshow('thresholded image 1',img)
 cv.waitKey(0)
 
-#now lets use the opencv built in function to threshold the image
-#before = time.time()
-#ret,copy = cv.threshold(copy,100,255,cv.THRESH_BINARY)
-#after = time.time()
-#print("Time taken to process opencv built in thresholding: " + str(after-before))
-#cv.imshow('thresholded image 2',copy)
-#cv.waitKey(0)
-#cv.destroyAllWindows()
+
 
 #read in an image into memory
 img = cv.imread('C:/Users/joshu/OneDrive/Documents/GitHub/ComputerVision/Orings/Oring2.jpg',0)
@@ -93,12 +85,37 @@ plt.xlabel('Pixel Value')
 plt.ylabel('Frequency')
 plt.show()
 
+#auto thresholding with otsus method from lecture 2
+hist = np.histogram(img.ravel(), 256, (0,256))[0]
+total = img.size
+sum_total = 0
+for t in range(256):
+    sum_total += t * hist[t]
+sumB = 0
+wB = 0
+varMax = 0
+threshold = 0
+for t in range(256):
+    wB += hist[t] #weight background
+    if wB == 0:
+        continue
+    wF = total - wB#weight foreground
+    if wF == 0:
+        break
+    sumB += t * hist[t]
+    mB = sumB / wB #mean background
+    mF = (sum_total - sumB) / wF #mean foreground
+    varBetween = wB * wF * (mB - mF) * (mB - mF) #between class variance
+    if varBetween > varMax:
+        varMax = varBetween
+        threshold = t
+print("auto thresholding", threshold)
+
 #implement thresholding ourselves using loops (soooo slow in python)
 before = time.time()
-thresh = 100
 for x in range(0, img.shape[0]):
     for y in range(0, img.shape[1]):
-        if img[x,y] > thresh:
+        if img[x,y] > threshold:
             img[x,y] = 255
         else:
             img[x,y] = 0
@@ -109,48 +126,3 @@ cv.waitKey(0)
 
 
 
-#read in an image into memory
-img = cv.imread('C:/Users/joshu/OneDrive/Documents/GitHub/ComputerVision/Orings/Oring3.jpg',0)
-copy = img.copy()
-#check out some of its pixel values...img[x,y]..try different x and y values
-x = 100
-y = 100
-pix = img[x,y]
-print("The pixel value at image location [" + str(x) + "," + str(y) + "] is:" + str(pix))
-
-#implement thresholding ourselves using loops (soooo slow in python)
-before = time.time()
-thresh = 100
-for x in range(0, img.shape[0]):
-    for y in range(0, img.shape[1]):
-        if img[x,y] > thresh:
-            img[x,y] = 255
-        else:
-            img[x,y] = 0
-after = time.time()
-print("Time taken to process hand coded thresholding: " + str(after-before))
-cv.imshow('thresholded image 3',img)
-cv.waitKey(0)
-
-#read in an image into memory
-img = cv.imread('C:/Users/joshu/OneDrive/Documents/GitHub/ComputerVision/Orings/Oring4.jpg',0)
-copy = img.copy()
-#check out some of its pixel values...img[x,y]..try different x and y values
-x = 100
-y = 100
-pix = img[x,y]
-print("The pixel value at image location [" + str(x) + "," + str(y) + "] is:" + str(pix))
-
-#implement thresholding ourselves using loops (soooo slow in python)
-before = time.time()
-thresh = 100
-for x in range(0, img.shape[0]):
-    for y in range(0, img.shape[1]):
-        if img[x,y] > thresh:
-            img[x,y] = 255
-        else:
-            img[x,y] = 0
-after = time.time()
-print("Time taken to process hand coded thresholding: " + str(after-before))
-cv.imshow('thresholded image 4',img)
-cv.waitKey(0)
